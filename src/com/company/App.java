0cefe676dfe2fbb2d60756d6ccfc7725514eabf8
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-01-01 16:06:16 -0600

    Edited the stdout a little more, put a blank method for calling other methods
    = need to implement logic now

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -1,35 +1,50 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class App
 {
-    public static final String[] features = new String[]{"grayscale_to_rgb","rgb_to_grayscale","grayscale_deepfry","invert","blur","line_detect"};
+    public static final String[] features = new String[]{"grayscale_to_rgb","rgb_to_grayscale","grayscale_deepfry","rgb_deepfry","invert","blur","line_detect"};
+
     public static void main(String[] args)
     {
         welcome();
         Scanner scan = new Scanner(System.in);
-        new Thread() {
-            public void run() {
-                boolean shouldContinue = true;
-                do {
-                    String nextCommand = scan.nextLine();
-                    if (nextCommand.equals("help")) displayFeatures();
-                    if (nextCommand.equals("end")) shouldContinue=false;
-                } while(shouldContinue);
-            }
-        }.start();
+
+        new Thread(() -> {
+            boolean shouldContinue = true;
+
+            do {
+                String nextCommand = scan.nextLine();
+                if (nextCommand.equals("help")) displayFeatures(); //if person wants help
+                else if (nextCommand.equals("end")) shouldContinue=false; //if person wants to end program
+                else
+                {
+                    processTasks(nextCommand);
+                }
+            } while(shouldContinue);
+        }).start();
     }
 
+    //Displays welcome text at start of program
     private static void welcome()
     {
-        System.out.println("To use this, enter the name of the feature you would like followed by the path of the image you would like to transform.");
-        System.out.println("-To see a full list of features, type \"help\" \n-To end the program, type \"end\"");
+        System.out.println("\nTo use this, enter the name of the feature you would like followed by the path of the image you would like to transform.");
+        System.out.println("-To see a full list of features, type \"help\" \n-To end the program, type \"end\"\n");
     }
 
+    //Prints an array with features of program
     private static void displayFeatures()
     {
         System.out.println("Features: " + Arrays.toString(features));
+        System.out.println("\"grayscale_to_rgb\" and vice-versa, and \"rgb_deepfry\" and vice versa, are self-explanatory");
+        System.out.println("-\"blur\" blurs an image. Can do multiple times to make picture very blurry");
+        System.out.println("-\"line_detect\" creates a new image outlining the edges of the picture inputted\n");
+    }
+
+    //Constructs objects and makes method calls to process the images
+    private static void processTasks(String nextLine)
+    {
+        //System.out.println(nextCommand);
     }
 }
