8fac106f4c8be2c6dc53881b3e2dfb2e8320b97a
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-01-01 15:27:00 -0600

    Adding starter code
    - added 'welcome' line and a method to detect whether the user wants to see the features

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -1,8 +1,26 @@
 package com.company;
 
-public class App {
+import java.util.Arrays;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	//put code here
+public class App
+{
+    public static final String[] features = new String[]{"grayscale_to_rgb","rgb_to_grayscale","grayscale_deepfry","invert","blur","line_detect"};
+    public static void main(String[] args)
+    {
+        welcome();
+        Scanner scan = new Scanner(System.in);
+        if (scan.nextLine().equals("help")) displayFeatures();
+    }
+
+    private static void welcome()
+    {
+        System.out.println("To use this, enter the name of the feature you would like followed by the path of the image you would like to transform.");
+        System.out.println("To see a full list of features, type \"help\"");
+    }
+
+    private static void displayFeatures()
+    {
+        System.out.println("Features: " + Arrays.toString(features));
     }
 }
