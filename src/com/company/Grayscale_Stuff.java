a19b2d3bf8d8981b3853427fa343e9f1f5e2fd09
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-01-02 17:43:08 -0600

    Wrote template code for grayscale_to_rgb

--- src/com/company/Grayscale_Stuff.java
+++ src/com/company/Grayscale_Stuff.java
@@ -11,26 +11,44 @@ public class Grayscale_Stuff
     private static String command = null;
     private static BufferedImage img = null;
     private static String fileName = null;
+    private static int height = 0;
+    private static int width = 0;
+    private static BufferedImage transformed = null;
 
     public Grayscale_Stuff(String command, BufferedImage img)
     {
         this.command = command;
         this.img = img;
+        height = this.img.getHeight();
+        width = this.img.getWidth();
     }
 
     //will process a grayscale picture and write a rgb picture to the images folder
     public static void grayscale_to_rgb()
     {
+        for (int h = 0; h < height; h++)
+        {
+            for (int w = 0; w < width; w++)
+            {
+                int rgb_total = img.getRGB(w,h);
+                Color newPixelColor = new Color(rgb_total,rgb_total,rgb_total);
+                transformed.setRGB(w,h,newPixelColor.getRGB());
+            }
+        }
 
+        File newFile = new File("images/grayscale_to_rgb");
+        try{
+            ImageIO.write(transformed,"jpeg",newFile);
+        } catch(Exception yeet){
+            System.out.println(yeet);
+        }
+
+        System.out.println("grayscale_to_rgb transformation successful! Check the images folder for a \"grayscale_to_rgb\" image\n");
     }
 
     //will process a rgb pic and return a grayscale image to the images folder
     public static void rgb_to_grayscale()
     {
-        int height = img.getHeight();
-        int width = img.getWidth();
-        BufferedImage transformed = img;
-
         for (int h = 0; h < height; h++)
         {
             for (int w = 0; w < width; w++)
